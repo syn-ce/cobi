63b42008b74fb184b0204e87a1892b0f8f7e8bde
Maximilian Amthor <amthormaximilian@gmail.com>
2023-12-02 00:47:37 +0100

    refactor counting into separate function

--- count_binary.c
+++ count_binary.c
@@ -38,26 +38,35 @@ void print_with_delim(uint64_t n, int leading_whitespace) {
     }
 }
 
-int main(int argc, char **argv) {
-    if (argc < 2) {
-        fprintf(stderr, "File name needs to be specified as input parameter.\n");
-        exit(1);
-    }
+struct bit_count {
+    uint64_t total;
+    uint64_t zeros;
+    uint64_t ones;
+};
 
+typedef struct bit_count bit_count;
+
+bit_count *count_bits(char *path) {
     FILE *file;
-    char *filename = argv[1];
 
     // Open file in binary mode
-    file = fopen(filename, "rb");
+    file = fopen(path, "rb");
+
+    bit_count *bit_c = malloc(sizeof(bit_count));
+    if (bit_c == NULL) perror("Error during malloc\n"), exit(1);
 
     if (file == NULL) {
-        perror("Error opening file");
-        return 1;
+        fprintf(stderr, "Error opening file %s\n", path);
+        return bit_c;
     }
 
-    uint64_t count_tot = 0;
-    uint64_t count_0 = 0;
-    uint64_t count_1 = 0;
+    bit_c->total = 0;
+    bit_c->zeros = 0;
+    bit_c->ones = 0;
+
+    bit_c->total = 0;
+    bit_c->zeros = 0;
+    bit_c->ones = 0;
 
     // Read the file byte by byte
     uint8_t byte_masks[] = {1, 2, 4, 8, 16, 32, 64, 128};
@@ -66,40 +75,47 @@ int main(int argc, char **argv) {
     while ((byte = fgetc(file)) != EOF) {
         // Count ones in byte
         for (int i = 0; i < 8; i++) {
-            if (byte & byte_masks[i]) count_1++;
-            else count_0++;
+            if (byte & byte_masks[i]) bit_c->ones++;
+            else bit_c->zeros++;
         }
-        count_tot += 8;
+        bit_c->total += 8;
     }
 
     // Close file
     fclose(file);
 
+    return bit_c;
+}
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        fprintf(stderr, "File name needs to be specified as input parameter.\n");
+        exit(1);
+    }
+
+    bit_count *bit_c = count_bits(argv[1]);
+
     // Calc 0:1 ratio
     double ratio01;
-    if (count_1 == 0) ratio01 = 0.0;
-        else ratio01 = count_0 / (double) count_1;
+    if (bit_c->ones == 0) ratio01 = 0.0;
+        else ratio01 = bit_c->zeros / (double) bit_c->ones;
 
-    double share0, share1;
-    if (count_tot == 0) share0 = 0, share1 = 0;
-        else share0 = count_0 / (double) count_tot, share1 = count_1 / (double) count_tot;
+    double share0, share1; 
+    if (bit_c->total == 0) share0 = 0, share1 = 0;
+        else share0 = bit_c->zeros / (double) bit_c->total, share1 = bit_c->ones / (double) bit_c->total;
 
-    int max_nr_digits = nr_digits(count_tot);
-
-    int digits_until_next_whitespace = max_nr_digits % 3;
-    if (digits_until_next_whitespace == 0) digits_until_next_whitespace = 3;
-
+    int max_nr_digits = nr_digits(bit_c->total);
 
     printf("B: ");
-    print_with_delim(count_tot, 0);
+    print_with_delim(bit_c->total, 0);
     printf("\n");
 
     printf("0: ");
-    print_with_delim(count_0, max_nr_digits - nr_digits(count_0));
+    print_with_delim(bit_c->zeros, max_nr_digits - nr_digits(bit_c->zeros));
     printf("  (%lf)\n", share0);
 
     printf("1: ");
-    print_with_delim(count_1, max_nr_digits - nr_digits(count_1));
+    print_with_delim(bit_c->ones, max_nr_digits - nr_digits(bit_c->ones));
     printf("  (%lf)\n", share1);
 
     return 0;
